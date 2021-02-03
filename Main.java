819c0514c126b94c84c2310978127923ba420fa7
110560796CW <110560796@ea.edin.sch.uk>
2021-02-03 11:46:00 +0000

    the code

--- Main.java
+++ Main.java
@@ -1,5 +1,22 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    int length = Keyboard.getInt(" how long is the football pitch?");
+    while (length < 90 || length > 120){
+      Screen.display(" this pitch does not meet regulations","length thing");
+
+    }
+    
+     int width = Keyboard.getInt("how wide is the football pitch?");
+     while (width < 45 || width > 90){
+       Screen.display(" this football pitch no-longer meets regulations","width thing");
+     }
+     
+
+    
+
+
+
+
+    
   }
 }
